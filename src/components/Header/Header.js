c782fa137e1e7bef707f0abe8d949c653bae7cf6
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-05-01 11:07:31 +0700

    membuat header, head, navbar component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+const Header = props => {
+    return (
+        <header>
+            <div>
+                <h2>Color Guess Game</h2>
+                <h1>RGB (147, 280, 67)</h1>
+                <h2>Guess the color above!</h2>
+            </div>
+        </header>
+    );
+}
+ 
+export default Header;
