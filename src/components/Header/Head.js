46f196ccfaf7bed76a255e889913d970d9054fed
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-05-02 21:00:59 +0700

    memberi style pada head ketika screen lg & xl

--- src/components/Header/Head.js
+++ src/components/Header/Head.js
@@ -10,9 +10,9 @@ const Head = props => {
     }
 
     return (
-        <div className="w-full text-white py-2 flex flex-col items-center">
-            <h2 className="text-2xl font-semibold sm:text-3xl">Color Guess Game</h2>
-            <h1 className="text-4xl font-bold mt-2 sm:text-5xl">{getOneColor()}</h1>
+        <div className="w-full text-white py-2 flex flex-col items-center lg:py-4">
+            <h2 className="text-2xl font-semibold sm:text-3xl lg:mb-3 lg:text-4xl">Color Guess Game</h2>
+            <h1 className="text-4xl font-bold mt-2 sm:text-5xl lg:mb-2">{getOneColor()}</h1>
             <h2 className="text-xl mt-1 sm:text-2xl">Guess the color above!</h2>
         </div>
     );
