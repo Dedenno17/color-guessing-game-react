c0269c76ab92e7b8fec98bddaf05d72467351e88
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-05-04 16:20:33 +0700

    membuat pesudo class active pada div color

--- src/components/Main/CardColor.js
+++ src/components/Main/CardColor.js
@@ -1,17 +1,18 @@
 import { useSelector } from "react-redux";
 
-const CardColor = props => {
+const CardColor = (props) => {
+  const rightAnswer = useSelector((state) => state.isRightAnswer.isRightAnswer);
 
-    const rightAnswer = useSelector(state => state.isRightAnswer.isRightAnswer);
+  return (
+    <div
+      id={props.id}
+      className={`cursor-pointer w-full mt-3.5 mb-1 h-36 shadow-xl sm:rounded-lg md:w-80 md:rounded-lg md:h-40 xl:w-96 active:scale-95 ${
+        rightAnswer ? "pointer-events-none" : ""
+      }`}
+      style={{ backgroundColor: `${props.color}` }}
+      onClick={() => props.onChoose(props.color)}
+    ></div>
+  );
+};
 
-    return (
-        <div
-            id={props.id}
-            className={`cursor-pointer w-full mt-3.5 mb-1 h-36 shadow-xl sm:rounded-lg md:w-80 md:rounded-lg md:h-40 xl:w-96 ${rightAnswer ? 'pointer-events-none': ''}`}
-            style={{backgroundColor: `${props.color}`}}
-            onClick={() => props.onChoose(props.color)}
-        ></div>
-    );
-}
- 
 export default CardColor;
