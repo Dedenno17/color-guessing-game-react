cbd27598e152b3542e810889c072cca49d9f03c8
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-05-07 10:04:29 +0700

    menambah padding atas main componenet

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -42,7 +42,7 @@ const Main = (props) => {
   };
 
   return (
-    <div className="container mx-auto w-full bg-transparent pt-36 pb-2 flex justify-evenly flex-col items-center sm:pt-44 md:flex-row md:flex-wrap md:justify-evenly md:pt-48 md:pb-10 lg:max-w-6xl lg:pt-64 xl:max-w-full">
+    <div className="container mx-auto w-full bg-transparent pt-44 pb-2 flex justify-evenly flex-col items-center sm:pt-44 md:flex-row md:flex-wrap md:justify-evenly md:pt-48 md:pb-10 lg:max-w-6xl lg:pt-64 xl:max-w-full">
       {colors.map((item) => (
         <CardColor
           key={item.id}
